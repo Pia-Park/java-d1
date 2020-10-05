49984a46a3d3f6534eb731427c01681f63b5995f
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2020-10-05 10:39:24 -0700

    first commit

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,15 @@
+public class MyFirstJavaProgram {
+    
+    public static void main(String[] args){
+        byte smallNumber = 23;
+        short slightlyBiggerNumber = 230;
+
+        System.out.println(smallNumber);
+        System.out.println(slightlyBiggerNumber);
+
+        smallNumber = 27;
+
+        System.out.println(smallNumber);
+        System.out.println(smallNumber + slightlyBiggerNumber);
+    }
+}
